c1a33aeffb4600bd82536036209adfbba8f9d5cc
Ammar Farhan Mohamad Rizam <ammarfmr11@gmail.com>
2022-11-14 00:39:31 +0800

    Add script for adding interactive commands to testing server

---
+++ deploy-commands.js
@@ -0,0 +1,28 @@
+const { REST, Routes } = require("discord.js");
+const fs = require("node:fs");
+require("dotenv").config();
+
+const commands = [];
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        const data = await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (err) {
+        console.error(err);
+    }
+})();
